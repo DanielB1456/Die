98e30d4bab778d83a2ede86f28bdfe0404212ef0
DanielB1456 <90276425+DanielB1456@users.noreply.github.com>
2022-01-12 21:40:53 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "/src/App.js",
+    "/src/index.css",
+    "/src/index.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
